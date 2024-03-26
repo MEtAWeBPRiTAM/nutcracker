c33bf4e4aac1ae078b4558c8636c8f6aab547750
Kamlesh Soni <ramwebdesigning@gmail.com>
2024-03-26 17:54:42 +0530

    a

--- pages/api/increment-view-count.js
+++ pages/api/increment-view-count.js
@@ -1,38 +1,42 @@
-// pages/api/increment-view-count.js
-
-import client from "../../lib/db";
+import { getSession } from 'next-auth/client';
+import { connectToDatabase } from '../../utils/db';
+import { ObjectId } from 'mongodb';
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const { videoId } = req.query;
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
 
-      // Validate videoId
-      if (!videoId) {
-        return res.status(400).json({ error: "Video ID is required" });
-      }
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
 
-      // Update view count in the database
-      const db = client.db("nutCracker");
-      const videoCollections = db.collection("videosRecord");
-      const result = await videoCollections.updateOne(
-        { fileUniqueId: videoId },
-        { $inc: { viewCount: 1 } } // Increment view count by 1
-      );
+  const { videoId } = req.body;
 
-      if (result.modifiedCount !== 1) {
-        return res.status(404).json({ error: "Video not found" });
-      }
+  try {
+    const { db } = await connectToDatabase();
 
-      res.status(200).json({ success: true });
+    // Update view count in videosRecord collection
+    const updatedVideo = await db.collection('videosRecord').findOneAndUpdate(
+      { fileUniqueId: videoId },
+      { $inc: { viewCount: 1 } },
+      { returnOriginal: false }
+    );
 
-      
-    } catch (error) {
-      console.error("Error incrementing view count:", error);
-      res.status(500).json({ error: "Error incrementing view count" });
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    // Fetch related user from video information
+    const relatedUser = await db.collection('userRecord').findOne({ _id: updatedVideo.relatedUserId });
+
+    // Update view count in userRecord collection and totalViews
+    await db.collection('userRecord').updateOne(
+      { _id: relatedUser._id },
+      { $inc: { totalViews: 1 } }
+    );
+
+    console.log("Increment success");
+    return res.status(200).json({ message: 'View count incremented successfully' });
+  } catch (error) {
+    console.error('Error incrementing view count:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 }
