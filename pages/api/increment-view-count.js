3c7d123010fc7b0bd191c51b8a0f722120fcebf0
Kamlesh Soni <ramwebdesigning@gmail.com>
2024-03-26 18:00:27 +0530

    Update increment-view-count.js

--- pages/api/increment-view-count.js
+++ pages/api/increment-view-count.js
@@ -1,42 +1,38 @@
-import { getSession } from 'next-auth/client';
-import { connectToDatabase } from '../../lib/db';
-import { ObjectId } from 'mongodb';
+// pages/api/increment-view-count.js
+
+import client from "../../lib/db";
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  }
+  if (req.method === "POST") {
+    try {
+      const { videoId } = req.query;
 
-  const session = await getSession({ req });
-  if (!session) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
+      // Validate videoId
+      if (!videoId) {
+        return res.status(400).json({ error: "Video ID is required" });
+      }
 
-  const { videoId } = req.body;
+      // Update view count in the database
+      const db = client.db("nutCracker");
+      const videoCollections = db.collection("videosRecord");
+      const result = await videoCollections.updateOne(
+        { fileUniqueId: videoId },
+        { $inc: { viewCount: 1 } } // Increment view count by 1
+      );
 
-  try {
-    const { db } = await connectToDatabase();
+      if (result.modifiedCount !== 1) {
+        return res.status(404).json({ error: "Video not found" });
+      }
 
-    // Update view count in videosRecord collection
-    const updatedVideo = await db.collection('videosRecord').findOneAndUpdate(
-      { fileUniqueId: videoId },
-      { $inc: { viewCount: 1 } },
-      { returnOriginal: false }
-    );
+      res.status(200).json({ success: true });
 
-    // Fetch related user from video information
-    const relatedUser = await db.collection('userRecord').findOne({ _id: updatedVideo.relatedUserId });
-
-    // Update view count in userRecord collection and totalViews
-    await db.collection('userRecord').updateOne(
-      { _id: relatedUser._id },
-      { $inc: { totalViews: 1 } }
-    );
-
-    console.log("Increment success");
-    return res.status(200).json({ message: 'View count incremented successfully' });
-  } catch (error) {
-    console.error('Error incrementing view count:', error);
-    return res.status(500).json({ message: 'Internal Server Error' });
+      
+    } catch (error) {
+      console.error("Error incrementing view count:", error);
+      res.status(500).json({ error: "Error incrementing view count" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
