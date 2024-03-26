23a18694233affd8f32a81a33b08d52e084219ad
Kamlesh Soni <ramwebdesigning@gmail.com>
2024-03-26 17:05:51 +0530

    Update watch.js

--- watch.js
+++ watch.js
@@ -2,6 +2,7 @@ const chokidar = require('chokidar');
 const { exec } = require('child_process');
 
 const directoryToWatch = './public/uploads/';
+const commandToRestartServer2 = 'sudo systemctl reload nginx';
 const commandToRestartServer = 'pm2 restart 9';
 
 const watcher = chokidar.watch(directoryToWatch, {
@@ -9,19 +10,18 @@ const watcher = chokidar.watch(directoryToWatch, {
   persistent: true
 });
 
-watcher
-  .on('add', path => {
-    console.log(`File ${path} has been added`);
-    restartServer();
-  })
-  // .on('change', path => {
-  //   console.log(`File ${path} has been changed`);
-  //   restartServer();
-  // });
+watcher.on('add', path => {
+  console.log(`File ${path} has been added`);
+  restartServer();
+});
+// .on('change', path => {
+//   console.log(`File ${path} has been changed`);
+//   restartServer();
+// });
 
 function restartServer() {
-  exec(commandToRestartServer, (error, stdout, stderr) => {
-  console.log("Trying to restart");
+  exec(commandToRestartServer2, commandToRestartServer, (error, stdout, stderr) => {
+    console.log("Trying to restart");
     if (error) {
       console.error(`Error executing command: ${error}`);
       return;
